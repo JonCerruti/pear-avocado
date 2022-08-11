ba49b1955cc19f25b72a743fd7bae1f5b2ddc8ab
Jon <jonathancerruti@gmail.com>
2022-08-11 15:05:47 -0700

    express static change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/api/notes', (req, res) => {
@@ -51,9 +51,9 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'))
+// });
 
 app.listen(PORT, () =>{
   console.info(`app listening at http://localhost:${PORT} 🚀`)
